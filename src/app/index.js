6d7dd166240f6d0cbf4c8e2e3a45d9b3f955f8c9
ruslan <yrm.10111990@gmail.com>
2020-01-06 15:53:59 +0500

    Bootstrap css

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,15 @@
+///import bootstrap
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+///global variables
+import jQuery from 'jquery';
+window.$ = jQuery;
+
+///import modules
 import bar from "./js/example";
 
+///launch modules
 bar();
+
+///export modules
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: undefined !== process.env.NODE_ENV ? process.env.NODE_ENV : 'production',
+    plugins: [new MiniCssExtractPlugin({
+        filename: 'bundle.css',
+    })],
     entry: './app/index.js',
     output: {
         path: path.resolve(__dirname + '/', 'dist'),
@@ -11,7 +15,15 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: path.resolve(__dirname + '/', 'dist'),
+                        },
+                    },
+                    'css-loader'
+                ]
             }
         ]
     }
